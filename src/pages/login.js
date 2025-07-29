a96ae78ff6c5da7577554b091241e286cdd7f052
SoumojjalSen <soumojjalsen@gmail.com>
2025-07-29 09:55:30 +0530

    Refactor LoginPage layout: enhance responsiveness and improve text visibility with updated styles

--- src/pages/login.js
+++ src/pages/login.js
@@ -9,20 +9,39 @@ const GoogleLoginButton = dynamic(
 
 export default function LoginPage() {
   return (
-    <div className='w-screen min-h-screen flex justify-around items-center'>
-      <div className='w-4/12 flex justify-center items-center flex-col gap-4 '>
+    <div className='w-screen min-h-screen flex flex-col lg:flex-row justify-around items-center relative'>
+      {/* Background image for mobile, hidden on large screens */}
+      <div
+        className='absolute inset-0 lg:hidden z-0'
+        style={{
+          backgroundImage:
+            "url('https://res.cloudinary.com/dv6bqnxqf/image/upload/v1747337551/qwfm1ff8ocwdjkpsjn5f.jpg')",
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+        }}
+      >
+        {/* Overlay for better text visibility */}
+        <div className='absolute inset-0 bg-black opacity-60'></div>
+      </div>
+      {/* Content */}
+      <div
+        className='w-full lg:w-4/12 flex justify-center items-center flex-col gap-4 z-10 px-4 py-12 lg:py-0 bg-transparent lg:bg-[#183366]'
+        style={{ position: 'relative' }}
+      >
         <div className='flex justify-center items-center flex-col'>
-          <h1 className='text-3xl lg:text-6xl lg:font-medium font-semibold text-blue-400'>
+          <h1 className='text-5xl sm:text-6xl lg:text-6xl lg:font-medium font-semibold text-[#3ba3e6] drop-shadow-[0_2px_8px_rgba(0,0,0,0.9)] transition-all duration-200'>
             MediConnect
           </h1>
-          <h1 className='text-3xl lg:text-6xl lg:font-medium font-semibold text-blue-400'>
+          <h1 className='text-4xl sm:text-5xl lg:text-6xl lg:font-medium font-semibold text-white lg:text-blue-400 drop-shadow-[0_2px_8px_rgba(0,0,0,0.9)] transition-all duration-200'>
             Login
           </h1>
         </div>
         <GoogleLoginButton />
       </div>
+      {/* Right image for desktop */}
       <div
-        className='w-8/12 h-[100vh] rounded-l-3xl'
+        className='hidden lg:block w-8/12 h-[100vh] rounded-l-3xl z-0'
         style={{
           backgroundImage:
             "url('https://res.cloudinary.com/dv6bqnxqf/image/upload/v1747337551/qwfm1ff8ocwdjkpsjn5f.jpg')",
